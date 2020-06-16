5c781ffe59c5f401966c6b31cd912304737aaf26
Omaru Jawara <omaru.jawara94@gmail.com>
2020-06-16 10:18:24 -0400

    Log in View created with styling

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import RoutesContainer from '../components/routes/RoutesContainer';
-
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RoutesContainer from "../components/routes/RoutesContainer";
+import NavBarContainer from "../components/containers/NavBarContainer";
+import LoginContainer from "../components/containers/LoginContainer";
 class App extends Component {
   render() {
     return (
       <div className="app">
         <header className="app-header">
           <RoutesContainer />
+          <NavBarContainer />
+          <LoginContainer />
         </header>
       </div>
     );
--- src/store/utilities/allPlayers.js
+++ src/store/utilities/allPlayers.js
@@ -1,7 +1,6 @@
-// import axios from 'axios';
+import axios from "axios";
 
 // // ACTION TYPES;
-// const FETCH_ALL_PLAYERS = "FETCH_ALL_PLAYERS";
 
 // // ACTION CREATORS;
 // const fetchAllPlayers = players => {
@@ -12,22 +11,7 @@
 // }
 
 // // THUNK CREATORS;
-// export const fetchAllPlayersThunk = () => dispatch => {
-//   return axios
-//     .get('/api/players')
-//     .then(res => res.data)
-//     .then(players => dispatch(fetchAllPlayers(players)))
-//     .catch(err => console.log(err))
-// }
 
-// // REDUCER;
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case FETCH_ALL_PLAYERS:
-//       return action.payload;
-//     default:
-//       return state;
-//   }
-// }
+// // REDUCERS;
 
 // export default reducer;
